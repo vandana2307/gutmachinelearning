19a260d35bea00c043175ab80444a8b608f0c115
Jeya Vandana <jjv4001@med.cornell.edu>
2023-12-11 02:40:27 +0000

    Add SRA downloader script

---
+++ sra_downloader.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -x
+
+usage()  
+{  
+  echo "Usage: $0 <cellranger base path> <SRA file path> <destination directory>" 
+  exit 1  
+} 
+
+if [ $# -ne 3 ] ; then
+  usage
+else
+  cellranger_base_path=$1
+  sratoolkit_bin_path="${cellranger_base_path}/sratoolkit/sratoolkit.3.0.7-ubuntu64/bin"
+  reference_path="${cellranger_base_path}/reference/refdata-gex-GRCh38-2020-A"
+  cellranger_bin_path="${cellranger_base_path}/cellranger-7.2.0/bin"
+  sra_file_path=$2
+  dest_dir=$3
+
+  prefetch_cmd="${sratoolkit_bin_path}/prefetch"
+  fasterq_dump_cmd="${sratoolkit_bin_path}/fasterq-dump"
+  cellranger_cmd="${cellranger_bin_path}/cellranger"
+  mkdir -p "${dest_dir}/prefetch_output"
+  mkdir -p "${dest_dir}/fasterq_dumps"
+
+  while IFS= read -r line; do
+    cd "${dest_dir}/prefetch_output"
+    $prefetch_cmd ${line} ${dest_dir} "${dest_dir}/prefetch_output"
+    prefetch_dir=$(ls "${dest_dir}/prefetch_output")
+    $fasterq_dump_cmd --split-files ${prefetch_dir} --outdir "${dest_dir}/fasterq_dumps"
+    
+    ls "${dest_dir}/fasterq_dumps" | while IFS= read -r fasterqfile; do
+      pigz -p8 "${dest_dir}/fasterq_dumps/${fasterqfile}"
+    done
+
+    ls "${dest_dir}/fasterq_dumps" | while IFS= read -r gzipfile; do
+      echo ${gzipfile}
+      sample_name=$(echo ${gzipfile} | sed -r 's/(.*)_([0-9]+).fastq.gz/\1/g')
+      new_name=$(echo ${gzipfile} | sed -r 's/(.*)_([0-9]+).fastq.gz/\1_S1_L001_R\2_001.fastq.gz/g')
+      mkdir -p "${dest_dir}/gzip_files/${sample_name}"
+      mv "${dest_dir}/fasterq_dumps/${gzipfile}" "${dest_dir}/gzip_files/${sample_name}/${new_name}"
+    done
+
+    cd "${dest_dir}"
+    ls "${dest_dir}/gzip_files" | while IFS= read -r gzipdir; do
+      $cellranger_cmd count --id="run_count_${gzipdir}" \
+        --fastqs="${dest_dir}/gzip_files/${gzipdir}" \
+        --sample=${gzipdir} \
+        --transcriptome=${reference_path}
+    done
+
+    #rm -rf ${dest_dir}/prefetch_output/*
+    rm -rf ${dest_dir}/fasterq_dumps/*
+    rm -rf ${dest_dir}/gzip_files/*
+  done < "${sra_file_path}"
+
+fi
